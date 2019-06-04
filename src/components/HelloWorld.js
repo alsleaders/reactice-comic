241e2bf756e661e7a77dd3f4ceff3ca2c5180069
Amanda Leaders <alsleaders@gmail.com>
2019-06-04 15:29:43 -0400

    alt correct

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,9 +1,28 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-class HelloWorld extends Component {
-  render() {
-    return <h1>Hello, World!</h1>
-  }
+let RANDOM = Math.ceil(Math.random() * 2158)
+const API_URL = 'https://xkcd.now.sh'
+
+export default function HelloWorld() {
+  const [comic, setComic] = useState('')
+
+  useEffect(() => {
+    axios.get(`${API_URL}/${RANDOM}`).then(resp => {
+      console.log(resp.data)
+      setComic(resp.data)
+      console.log({ comic })
+    })
+  }, [])
+
+  return (
+    <section>
+      <div className="boxes">
+        <h1>XKCD</h1>
+      </div>
+      <div className="boxes">
+        <img src={comic.img} alt={comic.transcript} title={comic.alt} />
+      </div>
+    </section>
+  )
 }
-
-export default HelloWorld
