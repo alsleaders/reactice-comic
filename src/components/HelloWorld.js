b13b3b7723512c8e3eb97492871ce8fd034d51e0
Amanda Leaders <alsleaders@gmail.com>
2019-07-16 22:00:06 -0400

    working on portfolio

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 // let RANDOM = Math.ceil(Math.random() * 2158)
-const API_URL = 'https://xkcd.now.sh'
+const API_URL = 'https://xkcd.com'
 
 export default function HelloWorld() {
   const [comic, setComic] = useState('')
